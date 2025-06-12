11b55c2e012625f4a901b864559b158f40bb855d
Vaishnavi Giri <vaishnavigiri1210@gmail.com>
2025-06-12 19:27:26 +0530

    first commit on vscode

---
+++ src/main/java/com/git/github_second_project/GithubSecondProjectApplication.java
@@ -0,0 +1,13 @@
+package com.git.github_second_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubSecondProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubSecondProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/git/github_second_project/GithubSecondProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git.github_second_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubSecondProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
